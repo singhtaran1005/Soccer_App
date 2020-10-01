086213dff6089a858f9a2602426df2879d7d3d11
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-10-01 11:33:31 +0530

    updated

---
+++ lib/model/goalstat.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget goalStat(int expandedTime, int homeGoal, int awayGoal) {
+  var home = homeGoal;
+  var away = awayGoal;
+  var elapsed = expandedTime;
+  if (home == null) home = 0;
+  if (away == null) away = 0;
+  if (elapsed == null) elapsed = 0;
+  return Expanded(
+    
